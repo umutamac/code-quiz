a092de3a350fceedd8b7c05cbe9bf9ccc8271cbb
Umut Alptekin <amacalptekin@gmail.com>
2020-09-26 23:11:22 +0300

    setting up the timer, it needs to stop at zero but it doesn't. Start button now triggers the timer and makes the question visible

--- logic.js
+++ logic.js
@@ -1,6 +1,8 @@
 var hiScoreBtn = document.querySelector("#hi-scores")
 var startBtn = document.querySelector("#start-btn")
-
+var answerBtn = document.querySelector("#start-btn")
+var qa = document.getElementById("q&a-window")
+var time = document.getElementById("time");
 
 // var hiScoreBtn = document.querySelector("#hi-scores")
 // var hiScoreBtn = document.querySelector("#hi-scores")
@@ -8,6 +10,8 @@ var startBtn = document.querySelector("#start-btn")
 // var hiScoreBtn = document.querySelector("#hi-scores")
 
 // Questions
+qa.style.display="none";
+
 var questions = 
 ["What keyword is used to call a function when an element is clicked on?",
 "Which one is used to link a JS file to the HTML file?",
@@ -16,21 +20,38 @@ var questions =
 
 var answers = 
 [""
-
-
-
-
 ];
 
-var timeLeft = 0;
-hiScoreBtn.addEventListener("click", function(event) {
-    timeleft = 60;
-    
 
+// Countdown Timer
+
+var secondsLeft = 10;
+function countdown(){
     
-});
+    time.textContent = "";
+    time.textContent += secondsLeft;
+    secondsLeft--;
+    
+}
+if (secondsLeft<0){
+    var y = clearInterval(countdown);
+    time.textContent = "Done";
+}
+
+
+
+
 
 startBtn.addEventListener("click", function(event) {
+    var x = setInterval(countdown,1000)
+    qa.style.display="block"
+    if (secondsLeft==0){
+        qa.style.display="none"
+    }
+});
+
+hiScoreBtn.addEventListener("click", function(event) {
     
 });
+
 
