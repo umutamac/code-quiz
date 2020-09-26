35ee496bf1405c966ca666b3c2f2b6161ecad232
Umut.A <amacalptekin@gmail.com>
2020-09-25 19:21:26 -0500

    created base html elements and js variables and event listeners. Pushing an asset too.

---
+++ logic.js
@@ -0,0 +1,36 @@
+var hiScoreBtn = document.querySelector("#hi-scores")
+var startBtn = document.querySelector("#start-btn")
+
+
+// var hiScoreBtn = document.querySelector("#hi-scores")
+// var hiScoreBtn = document.querySelector("#hi-scores")
+// var hiScoreBtn = document.querySelector("#hi-scores")
+// var hiScoreBtn = document.querySelector("#hi-scores")
+
+// Questions
+var questions = 
+["What keyword is used to call a function when an element is clicked on?",
+"Which one is used to link a JS file to the HTML file?",
+"What syntax is used to create comments in JS?",
+"Which keyword is used for manipulating an array?",]
+
+var answers = 
+[""
+
+
+
+
+];
+
+var timeLeft = 0;
+hiScoreBtn.addEventListener("click", function(event) {
+    timeleft = 60;
+    
+
+    
+});
+
+startBtn.addEventListener("click", function(event) {
+    
+});
+
