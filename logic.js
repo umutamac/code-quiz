c2d1f02ab941f62b914a8943d4f4dc7876cad4e0
Umut Alptekin <amacalptekin@gmail.com>
2020-09-30 03:28:32 +0300

    result save page now saves score and names into local storage with same indexes for easy retrieval. Cannot get them to show up in scores list page

--- logic.js
+++ logic.js
@@ -1,24 +1,27 @@
-// var ScoresBtn = document.getElementById("scores-btn")
-var startBtn = document.getElementById("start-btn")
+var startBtn = document.getElementById("start-btn");
 var time = document.getElementById("time");
 
 var index = 0;
 var QAwindow = document.querySelector(".QA-window");
-var question = document.getElementById("question");
+var Question = document.getElementById("question");
 var answerBtnsDiv = document.querySelector(".answer-buttons-div");
 var feedback = document.querySelector(".feedback");
 
 var resultSavePage = document.querySelector(".result-save-page");
 var currentScore = document.getElementById("currentScore");
 var saveScoreBtn = document.getElementById("saveScoreBtn");
+var nameinput = document.getElementById("fname");
+var interval = 0; // introduce interval variable
+var secondsLeft = 0; // introduce remaining time variable
 
+// scores page variables
 var scoresPage = document.querySelector(".scores-page");
+var scoresList = document.querySelector("#scores-list");
 
 var nameinput = [];
 var scoreinput = [];
 
-var interval = 0; // introduce interval variable
-var secondsLeft = 0; // introduce remaining time variable
+
 
 var questionSets = [
     {question: "What keyword is used to call a function when an element is clicked on?",
@@ -35,11 +38,9 @@ var questionSets = [
     correct:"splice"},
 ];
 
-
-
 function showQuestionAndAnswers(index){
     answerBtnsDiv.innerHTML = ""; // empty out any existing answer buttons
-    question.textContent = questionSets[index].question; // fill in question text with 1st question
+    Question.textContent = questionSets[index].question; // fill in question text with 1st question
     QAwindow.prepend(question); // add question to the existing paragraph in html file
 
     for (var i=0; i<questionSets[index].answers.length; i++) {   
@@ -52,20 +53,6 @@ function showQuestionAndAnswers(index){
         }
     }
 }
-
-
-
-function resultEntry(){ 
-    currentScore.textContent = secondsLeft; // Display seconds left on score saving form
-}
-
-
-
-
-
-
-
-
 
 function countdown(){ // countdown starts at one second less than secondsLeft variable
     secondsLeft--;
@@ -85,9 +72,7 @@ function countdown(){ // countdown starts at one second less than secondsLeft va
 
 
 
-
-
-function init() {
+function scoresPageInitialize() {
     // Get stored todos from localStorage
     // Parsing the JSON string to an object
     var storednames = JSON.parse(localStorage.getItem("nameinput"));
@@ -100,43 +85,23 @@ function init() {
     if (storedscores !== null) {
         scoreinput = storedscores;
     }
-//     renderTodos();
-}
-var nameinput = document.getElementById("fname");
-
-function saveScore() {
-    // Stringify and set "todos" key in localStorage to todos array
-    localStorage.setItem("nameinput", JSON.stringify(nameinput));
-    localStorage.setItem("scoreinput", JSON.stringify(scoreinput));
+    renderScores();
 }
 
-function showScores(){  //when view scores is clicked,
-    clearInterval(interval); //stop timer
-    secondsLeft = 0;         //reset timer
-    scoresPage.style.display = "block"; // show scores page
-    QAwindow.style.display = "none"; //hide questions and answers
-    resultSavePage.style.display="none"; // hide result save form
-}
-
-
-saveScoreBtn.addEventListener("click", function(event) {
-    event.preventDefault();
+function renderScores() {
+    scoresList.innerHTML = "";
   
-    var todoText = todoInput.value.trim();
-  
-    // Return from function early if submitted todoText is blank
-    if (todoText === "") {
-      return;
+    // Render a new li for each score 
+    for (var i=0; i < scoreinput.length; i++) {
+      var score = scoreinput[i];
+      var name = nameinput[i];
+      var li = document.createElement("li");
+      li.textContent = "Name: " + name + " \- Score: " + score;
+      li.setAttribute("data-index", i);
+      scoresList.appendChild(li);
     }
-  
-    // Add new todoText to todos array, clear the input
-    todos.push(todoText);
-    todoInput.value = "";
-  
-    // Store updated todos in localStorage, re-render the list
-    storeTodos();
-    renderTodos();
-});
+};
+
 
 
 
@@ -145,8 +110,8 @@ saveScoreBtn.addEventListener("click", function(event) {
 // Program the start button
 startBtn.addEventListener('click', function(){
     resultSavePage.style.display="none"; //hide score saving div
-    scoresPage.style.display = "none";  //hide scores list div
     feedback.textContent = ""; // empty out the text of feedback
+
     QAwindow.style.display = "block"; //Show question and answer div
     index = 0; //reset index to reset the questions
     showQuestionAndAnswers(index); // show first question and answer
@@ -160,15 +125,16 @@ startBtn.addEventListener('click', function(){
 document.body.addEventListener("click", function(event){
     var target = event.target;
     if (target.classList.contains("answerbutton")){
-        var isCorrect = target.getAttribute("data-correct");
-        if (isCorrect) {
+        var correctanswer = target.getAttribute("data-correct");
+        if (correctanswer) {
             feedback.textContent = "Correct!";
         }
-        if(!isCorrect){
+        if(!correctanswer){
             feedback.textContent = "Wrong. -5 seconds";
             secondsLeft = secondsLeft-5;
         }
-        index++;
+
+        index++; // move to next question set 
 
         if (index >= questionSets.length) { // if we run out of question sets,
         clearInterval(interval); // stop timer
@@ -180,7 +146,30 @@ document.body.addEventListener("click", function(event){
     }
 });
 
-// ScoresBtn.addEventListener("click",showScores);
+// program the save button after quiz
+saveScoreBtn.addEventListener("click", function(event) {
+    event.preventDefault(); // do not refresh page
+  
+    var name = fname.value.trim(); 
+  
+    // Return from function early if submitted name is blank
+    if (name === "") {
+      return;
+    }
+  
+    // Add new name and score with same indexes to array, clear the input
+    nameinput.push(name);
+    scoreinput.push(secondsLeft);
+    fname.value = "";
+
+    // Stringify and save score and name 
+    // with respective keys in localStorage
+    localStorage.setItem("nameinput", JSON.stringify(nameinput));
+    localStorage.setItem("scoreinput", JSON.stringify(scoreinput));
+
+});
 
 
+
+scoresPageInitialize();
 
